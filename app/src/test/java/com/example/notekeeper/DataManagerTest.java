5cdc93892e6c9c0eb2472b7f43606bb00ad0eb72
Oyedee <oyempemia@gmail.com>
2021-06-23 18:23:47 +0100

    Pre-processing between DataManagerTest class methods (createNewNote and findSimilarNotes)

--- app/src/test/java/com/example/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/example/notekeeper/DataManagerTest.java
@@ -1,10 +1,19 @@
 package com.example.notekeeper;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class DataManagerTest {
+    //This will always run before any test method in this class
+    @Before
+    public void setUp() {
+        DataManager dm = DataManager.getInstance(); //get reference to DataManager
+        dm.getNotes().clear();// clear out the list of notes
+        dm.initializeExampleNotes(); //add some example notes to the data manager after clearing it.
+        //This will make our test always start out with the same set of notes.
+    }
 
     @Test
     public void createNewNote() {
@@ -28,5 +37,35 @@ public class DataManagerTest {
         assertEquals(course, compareNote.getCourse());
         assertEquals(noteTitle, compareNote.getTitle());
         assertEquals(noteText, compareNote.getText());
+    }
+
+    @Test
+    public void findSimilarNotes() {
+        DataManager dm = DataManager.getInstance();
+        final CourseInfo course = dm.getCourse("android_async");
+        final String noteTitle = "Test note title";
+        final String noteText1 = "This is the body text of my test note";
+        final String noteText2 = "This is the second body text of my note";
+
+
+        int noteIndex1 = dm.createNewNote();
+        NoteInfo newNote1 = dm.getNotes().get(noteIndex1);
+        newNote1.setCourse(course);
+        newNote1.setTitle(noteTitle);
+        newNote1.setText(noteText1);
+
+        int noteIndex2 = dm.createNewNote();
+        NoteInfo newNote2 = dm.getNotes().get(noteIndex2);
+        newNote2.setCourse(course);
+        newNote2.setTitle(noteTitle);
+        newNote2.setText(noteText2);
+
+        //find the note index for the first new note and compare
+        int findNoteIndex1 = dm.findNote(newNote1);
+        assertEquals(findNoteIndex1, noteIndex1);
+
+        //find the note index for the second new note and compare
+        int findNoteIndex2 = dm.findNote(newNote2);
+        assertEquals(findNoteIndex2, noteIndex2);
     }
 }
