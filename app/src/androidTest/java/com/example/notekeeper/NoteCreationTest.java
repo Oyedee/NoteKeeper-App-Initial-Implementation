3248788415c22d251ca240e8bd455f47aabc9f2a
Oyedee <oyempemia@gmail.com>
2021-06-26 19:59:15 +0100

    implementing Spinner and Back button interactions - onData and pressBack methods

--- app/src/androidTest/java/com/example/notekeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/example/notekeeper/NoteCreationTest.java
@@ -5,6 +5,7 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,9 +15,19 @@ import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static org.hamcrest.Matchers.*;
+import static androidx.test.espresso.Espresso.pressBack;
+
 
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
+    //add DataManager static field
+    static DataManager sDataManager;
+    //initialize DataManager instance
+    @BeforeClass
+    public static void classSetup() {
+        sDataManager = DataManager.getInstance();
+    }
     //launch Test Activity
     @Rule
     public ActivityScenarioRule<NoteListActivity> mNoteListActivity =
@@ -24,13 +35,33 @@ public class NoteCreationTest {
 
     @Test
     public void createNewNote() {
+        //Let's get a reference to our variables
+        final CourseInfo course = sDataManager.getCourse("java_lang");
+        final String noteTitle = "This is the note Title";
+        final String noteText = "This is the body of the text";
+
 //        ViewInteraction fabNewNote = onView(withId(R.id.fab));
 //        fabNewNote.perform(click());
 
         //we can also write the above class this way
         onView(withId(R.id.fab)).perform(click());
-        onView(withId(R.id.text_note_title)).perform(typeText("This is the note Title"));
-        onView(withId(R.id.text_note_text)).perform(typeText("This is the body of the text"), closeSoftKeyboard());
+
+        //let's perform a click on our spinner before selecting our course with onData
+        onView(withId(R.id.spinner_courses)).perform(click());
+
+        //expanding our test to make use of the onData method and the pressBack method
+        //we use the onData method to select a course from the spinner
+        //When we use the onData method, rather than use view matchers, we use the hamcrest view matchers instead
+        onData(allOf(instanceOf(CourseInfo.class), equalTo(course))).perform(click());//look through the AdapterView on this activity and then find the one that holds CourseInfo instance who is equal to our course variable. That will then go into our spinner and find the course that correspond.
+
+        onView(withId(R.id.text_note_title)).perform(typeText(noteTitle));
+        onView(withId(R.id.text_note_text)).perform(typeText(noteText), closeSoftKeyboard());
+
+
+        //we use the pressBack method to return from NoteActivity to our NoteListActivity when we are done creating our new note
+        pressBack();
+
+
 
     }
 
