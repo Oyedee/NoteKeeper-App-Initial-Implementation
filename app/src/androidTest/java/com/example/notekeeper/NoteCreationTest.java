09256bee6ab5166b6d416f1bfa2792d8ed30df74
Oyedee <oyempemia@gmail.com>
2021-06-26 18:53:38 +0100

    setting up and implementing basic instrumentation test interactions

--- app/src/androidTest/java/com/example/notekeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/example/notekeeper/NoteCreationTest.java
@@ -1,5 +1,6 @@
 package com.example.notekeeper;
 
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
@@ -9,15 +10,27 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
+    //launch Test Activity
     @Rule
     public ActivityScenarioRule<NoteListActivity> mNoteListActivity =
             new ActivityScenarioRule<>(NoteListActivity.class);
 
     @Test
     public void createNewNote() {
+//        ViewInteraction fabNewNote = onView(withId(R.id.fab));
+//        fabNewNote.perform(click());
+
+        //we can also write the above class this way
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.text_note_title)).perform(typeText("This is the note Title"));
+        onView(withId(R.id.text_note_text)).perform(typeText("This is the body of the text"), closeSoftKeyboard());
 
     }
 
