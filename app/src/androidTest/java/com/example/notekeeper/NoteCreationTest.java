e217c4d34054307e9d4370d17e9ebd5da71aa5c2
Oyedee <oyempemia@gmail.com>
2021-06-27 19:44:11 +0100

    Verifying new note creation behaviour and implementing test verification

--- app/src/androidTest/java/com/example/notekeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/example/notekeeper/NoteCreationTest.java
@@ -10,6 +10,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static org.junit.Assert.*;
 import static androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.action.ViewActions.*;
@@ -54,12 +55,30 @@ public class NoteCreationTest {
         //When we use the onData method, rather than use view matchers, we use the hamcrest view matchers instead
         onData(allOf(instanceOf(CourseInfo.class), equalTo(course))).perform(click());//look through the AdapterView on this activity and then find the one that holds CourseInfo instance who is equal to our course variable. That will then go into our spinner and find the course that correspond.
 
+        //checks UI behaviour for spinner
+        onView(withId(R.id.spinner_courses)).check(matches(withSpinnerText(containsString(course.getTitle()))));
+
         onView(withId(R.id.text_note_title)).perform(typeText(noteTitle));
+        //checks UI behaviour for note title
+        onView(withId(R.id.text_note_title)).check(matches(withText(containsString(noteTitle))));
+
         onView(withId(R.id.text_note_text)).perform(typeText(noteText), closeSoftKeyboard());
 
+        //checks UI behaviour for note text
+        onView(withId(R.id.text_note_text)).check(matches(withText(containsString(noteText))));
 
         //we use the pressBack method to return from NoteActivity to our NoteListActivity when we are done creating our new note
         pressBack();
+
+        //checks Logic Behaviour. Let's make sure the logic of creating a note is run properly
+        //Let's confirm whatever note is the last note in the list, corresponds to the note values that we put in the input screen
+        //Let's get the index of the last note in the list
+        int noteIndex = sDataManager.getNotes().size() - 1;
+        NoteInfo note = sDataManager.getNotes().get(noteIndex);//use noteIndex to get note
+        assertEquals(course, note.getCourse());
+        assertEquals(noteTitle, note.getTitle());
+        assertEquals(noteText, note.getText());
+
 
 
 
